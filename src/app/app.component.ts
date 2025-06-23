606942bd7c1749cbe510dc8a25d4892c086f8eaa
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-06-23 12:54:36 -0300

    Finish Optimized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,19 @@ import lgThumbnail from 'lightgallery/plugins/thumbnail';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [MatTooltipModule,CommonModule],
+  imports: [MatTooltipModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit, AfterViewInit {
   title = 'ivanPage';
   mostrarTodas = false;
+  galeria: any;
+  imagenes = Array.from({ length: 26 }, (_, i) => ({
+    thumb: `assets/img/trabajo${i + 1}-thumb.avif`,
+    full: `assets/img/trabajo${i + 1}-full.avif`
+  }));
 
-  imagenes = Array.from({ length: 26 }, (_, i) => `assets/img/trabajo${i + 1}.avif`);
 
   ngOnInit() {
     AOS.init({
@@ -26,9 +30,28 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    lightGallery(document.getElementById('lightgallery')!, {
+    this.initGallery();
+  }
+
+  toggleVerMas(): void {
+    this.mostrarTodas = !this.mostrarTodas;
+    setTimeout(() => {
+      this.destroyGallery();
+      this.initGallery();
+    }, 100); // esperamos a que Angular renderice el nuevo contenido
+  }
+
+  initGallery(): void {
+    this.galeria = lightGallery(document.getElementById('lightgallery')!, {
       plugins: [lgZoom, lgThumbnail],
       speed: 500
     });
+  }
+
+  destroyGallery(): void {
+    if (this.galeria) {
+      this.galeria.destroy();
+      this.galeria = null;
+    }
   }
 }
